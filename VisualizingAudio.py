518202b8f2d3ad0271383c1cb7093f317d13e284
takumi-nishimura <clp13218@nitech.jp>
2022-04-08 00:00:39 +0900

    alpha

--- VisualizingAudio.py
+++ VisualizingAudio.py
@@ -47,9 +47,11 @@ class AUDIOINPUT:
                         stream_callback=self.callback)
 
     def callback(self, in_data, frame_count, time_info, statuss):
-        self.buffer = np.frombuffer(in_data, dtype='int16')
+        self.buffer = np.frombuffer(in_data, dtype='int16')/32768.0
         self.audio_array = np.append(self.audio_array,self.buffer)
         self.fft()
+        self.amp = np.abs(self.F/(self.CHUNK/2))
+        self.vol = sum(self.amp[25:128])
         return (in_data, pyaudio.paContinue)
 
     def close(self):
@@ -68,19 +70,43 @@ class PLOT:
         self.d1y = np.array([])
         self.d2x = np.fft.fftfreq(self.chunk,d=1.0/self.rate)
         self.d2y = np.array([])
+        self.d2ym = np.array([])
+        self.d3y = 0
+        self.d3ym = 0
+
         self.maxfft = np.zeros(self.rate)
 
     def visualize(self):
-        self.grid_x = 2
+        if self.d3y > self.d3ym:
+            self.m_t = time.perf_counter()
+            self.d2ym = self.d2y
+            self.d3ym = self.d3y
+        if time.perf_counter() - self.m_t > 2:
+            self.d3ym = 0
+
+        self.grid_x = 3
         self.grid_y = 2
 
         plt.clf()
 
         plt.subplot2grid((self.grid_x,self.grid_y),(0,0),colspan=self.grid_x)
-        plt.plot(self.d1x,self.d1y/32768.0)
-        plt.axis([0, 1, -1, 1])
+        plt.plot(self.d1x,self.d1y)
+        plt.axis([0, 1, -0.8, 0.8])
         plt.xlabel("time [sample]")
         plt.ylabel("amplitude")
+        
+        plt.subplot2grid((self.grid_x,self.grid_y),(2,0))
+        plt.plot(self.d2x[1:int(self.chunk/2)], self.d2y[1:int(self.chunk/2)])
+        plt.plot(self.d2x[1:int(self.chunk/2)], self.d2ym[1:int(self.chunk/2)], color='black', linestyle='dashed', linewidth = 1.0)
+        plt.axis([0, 500, 0, 0.1])
+        plt.xlabel("freq [Hz]")
+        plt.ylabel("amplitude")
+
+        plt.subplot2grid((self.grid_x,self.grid_y),(2,1))
+        plt.bar(1,self.d3y)
+        plt.hlines(self.d3ym, 0, 2, "blue", linestyles='dashed')
+        plt.axis([0, 2, 0, 1])
+        plt.xlabel("intensity")
 
         plt.pause(0.001)
 
@@ -95,7 +121,7 @@ class KEYINPUT:
             if kb == 's':
                 print('!!!start!!!')
                 if sleepflag == 0:
-                    time.sleep(0.5)
+                    time.sleep(0.8)
                     sleepflag = 1
                 else:
                     pass
@@ -128,6 +154,8 @@ if __name__ == "__main__":
                 while kt.is_alive():
                     try:
                         pl.d1y = ai.audio_array[-ai.rate:]
+                        pl.d2y = ai.amp
+                        pl.d3y = ai.vol
                         pl.visualize()
                         if ki.k_flag == 2:
                             print('reflesh')
