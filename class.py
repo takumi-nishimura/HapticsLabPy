4ac591730cc24770ad9dd47b91fbfcdf5b79566a
takumi-nishimura <clp13218@nitech.jp>
2022-04-15 16:09:34 +0900

    add class

---
+++ class.py
@@ -0,0 +1,20 @@
+from matplotlib import widgets
+
+
+class Human:
+    def __init__(self, name, height, weight) -> None:
+        self.name = name
+        self.height = height
+        self.weight = weight
+    
+    def hello(self):
+        print('name : ', self.name)
+        print('height : ', self.height)
+        print('weight : ', self.weight)
+        print('!!!hello!!!')
+
+yamada = Human('tanaka', 174, 75)
+yoshida = Human('yoshida', 164, 55)
+
+yamada.hello()
+yoshida.hello()
